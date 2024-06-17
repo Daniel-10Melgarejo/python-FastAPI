24a24a3f78574a7b79f67d8b4f444ee105d9bfc2
Daniel-10Melgarejo <oscar95d.melgarejo@gmail.com>
2024-06-17 11:28:31 -0400

    Segundo commit

--- Backend/FastAPI/main.py
+++ Backend/FastAPI/main.py
@@ -2,7 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+#Inicia el server: uvicorn Backend.FastAPI.main:app --reload
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return "Hello FastAPI"
+
+@app.get("/url")
+async def root():
+    return {" url_curso" : "http://mouredev.com/python"}
